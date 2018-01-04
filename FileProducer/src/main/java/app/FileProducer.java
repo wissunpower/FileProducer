1d4aadd111adde56b32c0ce957173b5a85bc4107
wissunpower <wissunpower@gmail.com>
2018-01-04 20:03:43 +0900

    - 설정 파일 기능 추가  1. Kafka Producer API 기본 제공 옵션 추가 ( broker Addr, port )  2. topic 이름 설정  3. 입력 파일 이름 설정

--- FileProducer/src/main/java/app/FileProducer.java
+++ FileProducer/src/main/java/app/FileProducer.java
@@ -1,12 +1,16 @@
 package app;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
@@ -20,25 +24,65 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.ss.util.CellReference;
 
 public class FileProducer {
+	
+	static final int CONFIG_FILENAME_ARGINDEX = 0;
+	static final String CONFIG_FILENAME_DEFAULT = "config.txt";
+	
+	static final String CONFIGNAME_TOPICNAME = "_topic_name";
+	static final String DEFAULTCONFIG_TOPICNAME = "my-topic";
+	static String configTopicName = DEFAULTCONFIG_TOPICNAME;
+
+	public static String getConfigTopicName() {
+		return configTopicName;
+	}
+
+	public static void setConfigTopicName(String configTopicName) {
+		FileProducer.configTopicName = configTopicName;
+	}
+
+	static final String CONFIGNAME_INPUTFILENAME = "_input_file_name";
+	static String configInputFileName = "";
+
+	public static String getConfigInputFileName() {
+		return configInputFileName;
+	}
+
+	public static void setConfigInputFileName(String configInputFileName) {
+		FileProducer.configInputFileName = configInputFileName;
+	}
 
 	public static void main(String[] args) throws EncryptedDocumentException, InvalidFormatException, IOException {
 		
-		// create producer
+		// producer default properties
 		Properties props = new Properties();
-		props.put( "bootstrap.servers", "localhost:9092" );
-		props.put( "acks", "all" );
-		props.put( "retries", 0 );
-		props.put( "batch.size", 16384 );
-		props.put( "linger.ms", 1 );
-		props.put( "buffer.memory", 33554432 );
-		props.put( "key.serializer", "org.apache.kafka.common.serialization.StringSerializer" );
-		props.put( "value.serializer", "org.apache.kafka.common.serialization.StringSerializer" );
+		props.put( ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092" );
+		props.put( ProducerConfig.ACKS_CONFIG, "all" );
+		props.put( ProducerConfig.RETRIES_CONFIG, 0 );
+		props.put( ProducerConfig.BATCH_SIZE_CONFIG, 16384 );
+		props.put( ProducerConfig.LINGER_MS_CONFIG, 1 );
+		props.put( ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432 );
+		props.put( ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer" );
+		props.put( ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer" );
 		
+		String configFileName = CONFIG_FILENAME_DEFAULT;
+		if ( args.length > CONFIG_FILENAME_ARGINDEX ) {
+			configFileName = args[ CONFIG_FILENAME_ARGINDEX ].toString();
+		}
+		try {
+			loadConfiguration( configFileName, props );
+		}
+		catch ( FileNotFoundException e ) {
+			System.out.println( "Not Found Configuration File. ( file name : " + configFileName + " )" );
+		}
+		catch ( IOException e ) {
+			System.out.println( "Invalid Configuration File Format. ( file name : " + configFileName + " )" );
+		}
+		
+		// create producer
 		Producer< String, String > producer = new KafkaProducer< String, String >( props );
-		String topicName = "my-topic";
 
 		// excel file read
-		InputStream inp = new FileInputStream( "open_dataset_list.xlsx" );
+		InputStream inp = new FileInputStream( getConfigInputFileName() );
 		
 		Workbook wb = WorkbookFactory.create( inp );
 
@@ -78,11 +122,38 @@ public class FileProducer {
 					System.out.println();
 				}
 				
-				producer.send( new ProducerRecord< String, String >( topicName, cellRef.formatAsString(), text ) );
+				producer.send( new ProducerRecord< String, String >( getConfigTopicName(), cellRef.formatAsString(), cellRef.formatAsString() + " - " + text ) );
 			}
 		}
 
 		producer.close();
 	}
 
+	public static void loadConfiguration( String fileName, Properties outProp ) throws IOException {
+		BufferedReader configFile = new BufferedReader( new FileReader( fileName ) );
+		String currentStr = null;
+		while ( ( currentStr = configFile.readLine() ) != null ) {
+			// empty line & comment check.
+			if ( currentStr.length() < 2 || currentStr.substring( 0, 2 ).equals( "//" ) )
+				continue;
+
+			final String name = currentStr.substring( 0, currentStr.indexOf( '=' ) ).trim();
+			final String value = currentStr.substring( currentStr.indexOf( '=' ) + 1 ).trim();
+			
+			if ( name.equals( CONFIGNAME_TOPICNAME ) ) {
+				setConfigTopicName( value );
+			}
+			else if ( name.equals( CONFIGNAME_INPUTFILENAME ) ) {
+				setConfigInputFileName( value );
+			}
+			else {
+				// Kafka Producer API 기본 설정 처리.
+				// 일단 모든 설정의 value를 String 형식으로 입력한다.
+				outProp.put( name, value );
+			}
+			
+			System.out.println( "name = " + name + ", value = " + value );
+		}
+		configFile.close();
+	}
 }
